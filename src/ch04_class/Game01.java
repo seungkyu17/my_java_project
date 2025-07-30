fd3e764794bc9602ab12318422f4920d22e469d0
ict <ict@naver.com>
2025-07-30 16:59:15 +0900

    first

--- src/ch04_class/Game01.java
+++ src/ch04_class/Game01.java
@@ -19,7 +19,9 @@ public class Game01 {
         }else{
             getRaceName = "오크";
         }
+        String message = name + "캐릭터는 정직한 스타일의 기사입니다";
+        return message;
     }
 
-    String message = name + "캐릭터는 정직한 스타일의 기사입니다";
+
 }
